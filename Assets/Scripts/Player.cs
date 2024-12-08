12934d6ac6bcefdb61e53fb8bbacd6db0cec68a5
Melike Seda Ketenci <115991225+MSedaK@users.noreply.github.com>
2024-12-08 20:00:11 +0300

    Proje kurulumu ve yeni player prefabi

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CapsuleCollider2D capsuleCollider { get; private set; }
+    public PlayerMovement movement { get; private set; }
+    public DeathAnimation deathAnimation { get; private set; }
+    public Animator animator;
+
+   
+    public bool dead => deathAnimation.enabled;
+    public bool starpower { get; private set; }
+
+    private void Awake()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        movement = GetComponent<PlayerMovement>();
+        deathAnimation = GetComponent<DeathAnimation>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void Hit()
+    {
+        if (!dead && !starpower)
+        {
+           
+                Death();
+           
+        }
+    }
+
+    public void Death()
+    {
+        deathAnimation.enabled = true;
+        animator.SetTrigger("Die");
+
+        GameManager.Instance.ResetLevel(3f);
+    }
+
+    public void Shrink()
+    {
+
+        capsuleCollider.size = new Vector2(1f, 1f);
+        capsuleCollider.offset = new Vector2(0f, 0f);
+
+        animator.SetBool("IsBig", false);
+        StartCoroutine(ScaleAnimation());
+    }
+
+    private IEnumerator ScaleAnimation()
+    {
+        float elapsed = 0f;
+        float duration = 0.5f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+    }
+
+    public void Starpower()
+    {
+        StartCoroutine(StarpowerAnimation());
+    }
+
+    private IEnumerator StarpowerAnimation()
+    {
+        starpower = true;
+
+        float elapsed = 0f;
+        float duration = 10f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            if (Time.frameCount % 4 == 0)
+            {
+            }
+
+            yield return null;
+        }
+
+        starpower = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    private bool grounded = false;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Hareket (Yön Tuþlarý)
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Sprite Flip
+        if (moveInput > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveInput < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Zýplama (Space)
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            Debug.Log("Zýplama baþladý!");
+        }
+
+     
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
+}
